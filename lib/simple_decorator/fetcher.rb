82d9f86cd377f7afabe9a6451ecc4e7f34ef36df
Nicolay Hvidsten <nico.hvi@gmail.com>
2014-01-20 11:22:53 +0100

    encode debugging

--- lib/simple_decorator/fetcher.rb
+++ lib/simple_decorator/fetcher.rb
@@ -9,7 +9,7 @@ class Fetcher
   def fetch(url)
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port, @opts[:proxy])
-    response = http.request(Net::HTTP::Get.new(uri.request_uri)).body#.encode('UTF-8', 'ISO-8859-1')
+    response = http.request(Net::HTTP::Get.new(uri.request_uri)).encode('UTF-8')#.encode('UTF-8', 'ISO-8859-1')
     p response
     response
   end
